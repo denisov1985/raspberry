7d6c9da33fee8a518130a560e4cc9404f688a630
Dmytro_Denysov <denisov1985@gmail.com>
2016-04-19 12:12:44 +0300

    Development in progress

--- Application/Cmd/velokievCommand.php
+++ Application/Cmd/velokievCommand.php
@@ -8,72 +8,89 @@ class VelokievCommand extends Command
 {
 
     private $db;
-    const LIST_URL = 'http://velokyiv.com/forum/viewforum.php?f=1';
+    const LIST_URL = 'http://velokyiv.com/forum/viewforum.php?f=';
 
     public function run()
     {
         $parser = new Parser();
-        $document = $parser->getDocument(self::LIST_URL);
-        $topicLinks = $document->get('.topictitle');
 
-        //echo count($topicLinks); die();
+        foreach ([1, 22, 177, 73] as $value) {
+            echo self::LIST_URL . $value . PHP_EOL;
+            $document = $parser->getDocument(self::LIST_URL . $value);
+            $topicLinks = $document->get('.topictitle');
 
-        foreach ($topicLinks as $topic) {
-            /** @var phpQueryObject $row */
+            //echo count($topicLinks); die();
 
-            $row = $topic->parents('li');
-            $class =$row->attr('class');
-            if (substr_count($class, 'announce') > 0 || substr_count($class, 'sticky')) {
-                continue;
+            foreach ($topicLinks as $topic) {
+                /** @var phpQueryObject $row */
+
+                $row = $topic->parents('li');
+                $class = $row->attr('class');
+                if (substr_count($class, 'announce') > 0 || substr_count($class, 'sticky')) {
+                    continue;
+                }
+                $href = $topic->attr('href');
+                $href = str_ireplace('./viewtopic.php', 'http://velokyiv.com/forum/viewtopic.php', $href);
+                $href = preg_replace('/&?sid=[^&]*/', '', $href);
+
+                $post = PostsModel::findBy(['post_link' => $href]);
+                if ($post) {
+                    continue;
+                }
+
+                echo $topic->text() . PHP_EOL;
+
+                $post = new PostsModel();
+                $post->setPostName($topic->text());
+                $post->setPostLink($href);
+                $post->setPostImage('');
+                $post->setPostDateTime('');
+                $post->setPostIsPublished('-1');
+                $post->setPostType('1');
+                $post->setGroupId('1');
+                $post->save();
             }
-            $href = $topic->attr('href');
-            $href = str_ireplace('./viewtopic.php', 'http://velokyiv.com/forum/viewtopic.php', $href);
-            $href = preg_replace('/&?sid=[^&]*/', '', $href);
 
-            $post = PostsModel::findBy(['post_link' => $href]);
-            if ($post) {
-                continue;
+            $posts = PostsModel::findBy(['post_is_published' => '-1']);
+            foreach ($posts as $post) {
+                $document = $parser->getDocument($post->getPostLink());
+                $content = $document->get('.content');
+                $content = $content[0]->html();
+                $content = str_ireplace('</p>', PHP_EOL, $content);
+                $content = str_ireplace('<br>', PHP_EOL, $content);
+                $content = str_ireplace('</br>', PHP_EOL, $content);
+                $content = strip_tags($content);
+                $content = explode('Реєстрація', $content);
+                $content = $content[0];
+
+                $date = explode(' ', $post->getPostName());
+                $date = @$date[0];
+                $date = explode('.', $date);
+                if (count($date) < 3) {
+                    $dateTime = '2000-01-01 00:00:00';
+                }   else  {
+                    $date = @$date[2] . '-' . @$date[1] . '-' . @$date[0];
+                    $time = explode('Старт: ', $content);
+                    $time = explode(PHP_EOL, @$time[1]);
+                    $time = trim(@$time[0]);
+                    $dateTime = $date . ' ' . $time . ':00';
+                }
+
+
+                if ($dateTime < date('Y-m-d H:i:s')) {
+                    $post->setPostIsPublished('1');
+                    echo $dateTime . ' not published ' . PHP_EOL;
+                }   else  {
+                    $post->setPostIsPublished('0');
+                    echo $dateTime . ' publisshed  ' . PHP_EOL;
+                }
+
+                $post->setPostDateTime($dateTime);
+                $post->setPostContent($content);
+                $post->save();
+                echo ".";
             }
-
-            $post = new PostsModel();
-            $post->setPostName($topic->text());
-            $post->setPostLink($href);
-            $post->setPostImage('');
-            $post->setPostDateTime('');
-            $post->setPostIsPublished('-1');
-            $post->setPostType('1');
-            $post->setGroupId('1');
-            $post->save();
         }
-
-        $posts = PostsModel::findBy(['post_is_published' => '-1']);
-        foreach ($posts as $post) {
-            $document = $parser->getDocument($post->getPostLink());
-            $content  = $document->get('.content');
-            $content = $content[0]->html();
-            $content = str_ireplace('</p>', PHP_EOL, $content);
-            $content = str_ireplace('<br>', PHP_EOL, $content);
-            $content = str_ireplace('</br>', PHP_EOL, $content);
-            $content =strip_tags($content);
-            $content = explode('Реєстрація', $content);
-            $content = $content[0];
-
-            $date = explode(' ', $post->getPostName());
-            $date = $date[0];
-            $date = explode('.', $date);
-            $date = $date[2] . '-' . $date[1] . '-' . $date[0];
-            $time = explode('Старт: ', $content);
-            $time = explode(PHP_EOL, $time[1]);
-            $time = trim($time[0]);
-            $dateTime = $date . ' ' . $time . ':00';
-
-            $post->setPostIsPublished('0');
-            $post->setPostDateTime($dateTime);
-            $post->setPostContent($content);
-            $post->save();
-            echo ".";
-        }
-
 
     }
 
