89fab10bad0e76a9942236adc24ac9c5ba531993
denisov1985 <denisov1985@gmail.com>
2016-05-05 14:34:15 +0300

    test

--- Test/actions.js
+++ Test/actions.js
@@ -3,8 +3,8 @@
  */
 var actions = {
     login: function() {
-        document.getElementById("email").value = "";
-        document.getElementById("pass").value = "";
+        document.getElementById("email").value = "makrosnik2@gmail.com";
+        document.getElementById("pass").value = "Rjpthju1";
         document.getElementById("loginbutton").click();
     }
 };
